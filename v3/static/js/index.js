1ebfd7191e6bcfd131a53e2fe47fd591f054171c
bw-vbnm <bwang19@illinois.edu>
2014-09-12 20:49:40 -0500

    improve with backstretch

--- v3/static/js/index.js
+++ v3/static/js/index.js
@@ -1,2 +1,9 @@
-$(document).ready(function(e) {
-}
+$("html").backstretch([
+	"static/img/bg1.jpg",
+    "static/img/bg2.jpg",
+   	"static/img/bg3.jpg"    
+], {duration: 0, fade: 450});
+
+setTimeout(function() { 
+	$("html").backstretch("pause");
+}, 1350);
