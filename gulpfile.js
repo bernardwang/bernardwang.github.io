2651c5abc78d97c306737517e42a3a1d271df597
bw-vbnm <bwang19@illinois.edu>
2014-12-25 13:53:03 -0500

    fix git ignore and stuff

--- gulpfile.js
+++ gulpfile.js
@@ -13,16 +13,16 @@ gulp.task('default', ['clean'], function() {
 });
 
 gulp.task('clean', function(cb) {
-    del(['dist/assets/css', 'dist/assets/js'], cb)
+    del(['assets/style/css', 'assets/js/*.min.js'], cb)
 });
 
 gulp.task('style', function() {
 	gulp.src('assets/style/sass/*.scss')
    		.pipe(sass())
 		.pipe(rename({suffix: '.min'}))
-	    .pipe(gulp.dest('dist/assets/css'))
+	    .pipe(gulp.dest('assets/style/css'))
 		.pipe(minifycss())
-	    .pipe(gulp.dest('dist/assets/css'))
+	    .pipe(gulp.dest('assets/style/css'))
 		.pipe(notify({ message: 'style complete' }));
 });
 
@@ -32,8 +32,8 @@ gulp.task('script', function() {
 	    .pipe(jshint.reporter('default'))
 	   	//.pipe(concat('script.js'))
 		.pipe(rename({suffix: '.min'}))
-		.pipe(gulp.dest('dist/assets/js'))
+		.pipe(gulp.dest('assets/js'))
 		.pipe(uglify())
-		.pipe(gulp.dest('dist/assets/js'))
+		.pipe(gulp.dest('assets/js'))
 		.pipe(notify({ message: 'script complete' }));
 });
