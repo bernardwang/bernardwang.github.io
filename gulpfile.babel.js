4c83fd8b80a176171362895e4b19a0a9ea94d3fb
bernardwang <bernard@bernard.wang>
2017-02-06 01:17:14 -0600

    update to new temporary site

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -28,7 +28,7 @@ const eslintOpts = {
 	//extends : 'eslint:recommended',
 	extends : 'airbnb',
 	parser : 'babel-eslint',
-  rules : {
+	rules : {
 		'indent': [2, 'tab']
 	}
 }
@@ -37,7 +37,7 @@ const autoprefixerOpts = {
 	add: true
 }
 const cssnanoOpts = {
-  autoprefixer: autoprefixerOpts
+	autoprefixer: autoprefixerOpts
 }
 const imageminOpts = {
 	progressive: true,
@@ -46,7 +46,7 @@ const imageminOpts = {
 	optimizationLevel : 3
 };
 const syncOpts = {
-  stream: true,
+	stream: true,
 };
 const deployOpts = {
 	branch: 'master', // user page website
@@ -72,14 +72,14 @@ const ENTRY_JS = './src/js/app.js';
 // Browserify function
 let bundler;
 function getBundler(watch) {
-  if (!bundler) { // Initialize bundle with conditional 'watch'
+	if (!bundler) { // Initialize bundle with conditional 'watch'
 		if (watch) {
-    	bundler = watchify(browserify(ENTRY_JS, watchifyOpts));
+		bundler = watchify(browserify(ENTRY_JS, watchifyOpts));
 		} else {
-    	bundler = browserify(ENTRY_JS, browserifyOpts);
+		bundler = browserify(ENTRY_JS, browserifyOpts);
 		}
-  }
-  return bundler;
+	}
+	return bundler;
 };
 
 /************ TASKS ************/
@@ -93,7 +93,7 @@ gulp.task('styles', () => {
 		.pipe(sass().on('error', sass.logError))
 		.pipe(autoprefixer(autoprefixerOpts))
 		.pipe(sourcemaps.write())
-    .pipe(gulp.dest(DEST_CSS))
+	.pipe(gulp.dest(DEST_CSS))
 		.pipe(sync.reload(syncOpts));
 });
 
@@ -103,30 +103,30 @@ gulp.task('styles', () => {
 gulp.task('min-styles', ['styles'], () => {
 	return gulp.src(DIST_CSS)
 		.pipe(cssnano(cssnanoOpts))
-    .pipe(gulp.dest(DEST_CSS));
+	.pipe(gulp.dest(DEST_CSS));
 });
 
 /**
  *	Builds JS persistently when needed
  */
 gulp.task('scripts-watch', () => {
-  return getBundler( true ) // Watchify
-    .transform(babelify) // Babelify options in package.json
+	return getBundler( true ) // Watchify
+	.transform(babelify) // Babelify options in package.json
 		.bundle().on('error', (err) => console.log('Error: ' + err.message))
-    .pipe(source('app.js'))	// Output name
-    .pipe(gulp.dest(DEST_JS))
-    .pipe(sync.reload(syncOpts));
+	.pipe(source('app.js'))	// Output name
+	.pipe(gulp.dest(DEST_JS))
+	.pipe(sync.reload(syncOpts));
 });
 
 /**
  *	Builds JS once
  */
 gulp.task('scripts', () => {
-  return getBundler( false ) // Not watchifying
-    .transform(babelify) // Babelify options in package.json
+	return getBundler( false ) // Not watchifying
+	.transform(babelify) // Babelify options in package.json
 		.bundle().on('error', (err) => console.log('Error: ' + err.message))
-    .pipe(source('app.js'))	// Output name
-    .pipe(gulp.dest(DEST_JS));
+	.pipe(source('app.js'))	// Output name
+	.pipe(gulp.dest(DEST_JS));
 });
 
 /**
@@ -135,16 +135,16 @@ gulp.task('scripts', () => {
 gulp.task('min-scripts', ['scripts'], () => {
 	return gulp.src(DIST_JS)
 		.pipe(uglify())
-    .pipe(gulp.dest(DEST_JS));
+	.pipe(gulp.dest(DEST_JS));
 });
 
 /**
  *	Lint JS
  */
 gulp.task('lint-scripts', () => {
-  gulp.src(SRC_JS)
-    .pipe(eslint(eslintOpts))
-    .pipe(eslint.format());
+	gulp.src(SRC_JS)
+	.pipe(eslint(eslintOpts))
+	.pipe(eslint.format());
 });
 
 /**
@@ -160,27 +160,27 @@ gulp.task('min-img', () => {
  *	Auto build and reload
  */
 gulp.task('dev', ['styles','scripts-watch'], () => {
-  sync({
-    server: {
-      baseDir: './dist/'
-    }
-  });
+	sync({
+	server: {
+		baseDir: './dist/'
+	}
+	});
 
 	// Reloads on HTML, CSS, and JS changes
 	gulp.watch(SRC_SASS, ['styles']);
 	gulp.watch(SRC_HTML).on('change', sync.reload);
-  getBundler().on('update', () => gulp.start('scripts-watch'));
+	getBundler().on('update', () => gulp.start('scripts-watch'));
 });
 
 /**
  *	Dist build
  */
 gulp.task('dist', ['min-img','min-styles','lint-scripts','min-scripts'], () => {
-  sync({
-    server: {
-      baseDir: './dist/'
-    }
-  });
+	sync({
+	server: {
+		baseDir: './dist/'
+	}
+	});
 });
 
 /**
