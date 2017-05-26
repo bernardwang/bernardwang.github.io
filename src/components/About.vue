b7c149ae6072f33888b30a797571c14ccf7a594b
bernardwang <bernard@bernard.wang>
2017-05-26 03:13:16 -0400

    Add resume, link still broken

--- src/components/About.vue
+++ src/components/About.vue
@@ -3,7 +3,7 @@
     h2.pl-fifth.f2.lh-title About
     div.cf
       aside.fl.w-100.w-20-ns
-        p.f6.lh-copy.w-90-ns Interested in my resume? Grab a copy here.
+        p.f6.lh-copy.w-90-ns Interested in my resume? Grab a copy #[a(href="./assets/img/resume.pdf" target="_blank" alt="resume") here]!.
       div.fl.w-100.w-80-ns
         div.fl.w-100.w-50-ns
           p.f5.lh-copy.w-90-ns I am excited about applying my background in Front-end Engineering and product design to social good. Specifically, I care about using technology to improve public works and empower grassroots organizing.
--- src/components/Contact.vue
+++ src/components/Contact.vue
@@ -3,21 +3,21 @@
     h2.pl-fifth.f2.lh-title Contact
     div.cf
       aside.fl.w-100.w-20-ns
-        p.f6.lh-copy.w-90-ns.mt4.pt1 I’m available for projects, lets work together!
+        p.f6.lh-copy.w-90-ns.mt4.pt1 I’m currently available for projects, lets work together!
       form(accept-charset="utf-8").fl.w-100.w-80-ns
         fieldset(action="msg_submit").ba.b--transparent.pa0.ma0.cf
           legend.ph0.mh0.fw6.clip Submit Message
           div.form-name.fl.w-100.w-50-ns.mt3
             label(for="name").f6.b.db.mb2 Name
-            input(id="name" placeholder="Cool person" type="text" name="name" aria-describedby="name-desc").w-90.pa2.ba.b--black.bg-transparent
+            input(id="name" placeholder="Cool person" type="text" name="name" aria-describedby="name-desc").w-90.pa3.ba.b--black.bg-transparent
           div.form-email.fl.w-100.w-50-ns.mt3
             label(for="email-address").f6.b.db.mb2 Email address
-            input(id="email" placeholder="cool@person.com" type="email" name="email-address" aria-describedby="email-desc").w-90.pa2.ba.b--black.bg-transparent
+            input(id="email" placeholder="cool@person.com" type="email" name="email-address" aria-describedby="email-desc").w-90.pa3.ba.b--black.bg-transparent
           div.form-msg.fl.w-100.mt3
             label(for="message").f6.b.db.mb2 Message
-            textarea(id="message" placeholder="Hey! blah blah!" name="message" aria-describedby="message-desc").pa2.mb2.ba.b--black.bg-transparent.w-100
+            textarea(id="message" placeholder="Hey! blah blah!" name="message" aria-describedby="message-desc").pa3.mb2.ba.b--black.bg-transparent.w-100
         div.mt3.w-100.w-50-ns
-          input(id="submit" value="SEND →" type="submit").b.ph3.pv2.ba.b--black.bg-transparent.pointer.f5.w-90
+          input(id="submit" value="SEND →" type="submit").b.pa3.ba.b--black.bg-transparent.pointer.f5.w-90
 </template>
 
 <script>
@@ -36,7 +36,7 @@ export default {
 }
 
 #message {
-
+  width: 95%;
 }
 
 #submit {
