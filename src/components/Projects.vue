9f427d7178e477e40fb7667ce14650507a2cae83
bernardwang <bernard@bernard.wang>
2017-05-25 23:28:07 -0400

    Project section scaffold

--- src/components/Projects.vue
+++ src/components/Projects.vue
@@ -1,13 +1,71 @@
 <template lang="pug">
-  section
-    h1 Projects
+  section.mt6
+    h1.pad-l.f2.lh-title Projects
+    div
+      article.mt5(v-for="proj in projects")
+        h1.pad-l.f3.lh-title.mb0 {{ proj.title }}
+        div.cf(v-for="(value, key) in proj.list" v-if="value")
+          aside.fl.w-100.w-20-ns
+            p.f6.lh-copy.w-90-ns.mb0 {{ key }}
+          div.fl.w-100.w-80-ns
+            p.f5.lh-copy.w-90-ns.mb0 {{ value }}
 </template>
 
 <script>
+const projects = [{
+  title: 'Explorations',
+  list: {
+    Expertise: 'Front-end / Web Design',
+    Details: 'Personal project showcasing my work in Art & Design classes throughout college. Uses Flickr to host images and a Handlebars to build the page.',
+    Credits: '',
+    Year: '2017'
+  }
+}, {
+  title: 'Lisa Vuong Portfolio',
+  list: {
+    Expertise: 'Front-end',
+    Details: 'Portfolio site for my dear friend and fellow shiba fan. Coded entirely in vanilla JS and Sass, the site   also uses custom built gallery sliders.',
+    Credits: 'Web Design: Lisa Vuong',
+    Year: '2015'
+  }
+}, {
+  title: 'IBM Maelstrom',
+  list: {
+    Expertise: 'Project Management / Web Design / Front-end',
+    Details: 'Website built for IBM Maelstrom, a ten week design program in Austin, TX. Part of a week long micro-project involving other branding and style deliverables.',
+    Credits: 'Web Design: Meme Betadam, Kei Yumino / Project Management: Tobi Oyadiran',
+    Year: '2016'
+  }
+}, {
+  title: 'This site!',
+  list: {
+    Expertise: 'Front-end / Web Design',
+    Details: 'Personal site and portfolio built with Vue.js, Webpack, Tachyons and PugJS. Visit archive.bernard.wang to view previous versions of this site!',
+    Credits: '',
+    Year: new Date().getFullYear()
+  }
+}]
+
 export default {
-  name: 'projects'
+  name: 'projects',
+  data () {
+    return {
+      projects
+    }
+  }
 }
 </script>
 
 <style lang="scss" scoped>
+// Fix later
+// Copied from tachyon -ns media query
+@media screen and (min-width: 30em) {
+  section{
+    padding-left: calc(100% / 6);
+  }
+
+  .pad-l{
+    padding-left: calc(100% / 5);
+  }
+}
 </style>
