f9ba99fcda492c730d07baa9cded747067f7d778
bernardwang <bernard@bernard.wang>
2017-06-01 15:47:12 -0700

    Finish grid

--- src/components/Grid.vue
+++ src/components/Grid.vue
@@ -1,10 +1,10 @@
 <template lang="pug">
   div#grid.w-100.h-100.ph-page.absolute.absolute--fill.cf
-    div.fl.w-100.w-third-ns.h-100.ba.b--near-white
-      div.fl.w-100.w-50-ns.h-100.ba.b--near-white
-      div.fl.w-100.w-50-ns.h-100.ba.b--near-white
-    div.fl.w-100.w-third-ns.h-100.ba.b--near-white
-    div.fl.w-100.w-third-ns.h-100.ba.b--near-white
+    div.fl.w-100.w-third-ns.h-100
+      span.fl.w-100.w-50-ns.h-100
+      div.fl.w-100.w-50-ns.h-100
+    div.fl.w-100.w-third-ns.h-100
+    div.fl.w-100.w-third-ns.h-100.end
 </template>
 
 <script>
@@ -14,7 +14,25 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-#grid{
+#grid {
   z-index: -1;
+
+  div {
+    border-left: 1px solid rgba(#FF548F, 0.1);
+    &.end {
+      border-right: 1px solid rgba(#FF548F, 0.1);
+    }
+  }
+}
+
+@media screen and (max-width: 30em) {
+  #grid {
+    div {
+      display: none;
+      &.end {
+        display: block;
+      }
+    }
+  }
 }
 </style>
