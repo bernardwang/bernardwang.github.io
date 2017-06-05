916ff4fe12647d7648690346c5a4d6f89bbee632
bernardwang <bernard@bernard.wang>
2017-06-05 01:04:23 -0700

    puppy

--- src/components/NotFound.vue
+++ src/components/NotFound.vue
@@ -2,8 +2,9 @@
   div.w-100.v-100.ph-page.relative
     main#notfound.pt5
       h1 Oops, theres's nothing here!
-      p Maybe you meant to go to my #[a(href="/" alt="homepage") homepage]?
-      p If something is broken please send me an email at #[a(href="mailto:bernard@bernard.wang" target="_blank" alt="email") bernard@bernard.wang] to get this sorted out!
+      video(autoplay loop)
+        source(src="https://media.giphy.com/media/tsIFjie7obBM4/giphy.mp4" type="video/mp4")
+      p Maybe you meant to go to my #[a(href="/" alt="homepage") homepage]? If not and something is broken, please send me an email at #[a(href="mailto:bernard@bernard.wang" target="_blank" alt="email") bernard@bernard.wang] to get this sorted out!
 </template>
 
 <script>
