9e5832695ad4acdc9b5d238fdfe225affeec1da2
Bernard Wang <bernard@bernard.wang>
2018-04-25 18:54:31 -0400

    Adjust fontsize on mobile

--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   section#intro
-    h1.w-80-l.w-100 Hey 👋, my name is Bernard! I'm a <span class="hl">frontend</span><span class="hl">engineer</span> and <span class="hl">designer</span> based in NYC.<br/>
+    h1.w-80-l.w-100.display Hey 👋, my name is Bernard! I'm a <span class="hl">frontend</span><span class="hl">engineer</span> and <span class="hl">designer</span> based in NYC.<br/>
     p.f4.w-90-l.w-100.mt5.mb0 Currently building political organizing tools at #[a(href="http://grove.ai" target="_blank" alt="Grove.ai") Grove.ai].
     p.f4.w-90-l.w-100.mt0 Say hi on #[a(href="https://twitter.com/_bernardwang/" target="_blank" alt="twitter") Twitter] or email me at #[a(href="mailto:bw.vbnm@gmail.com" target="_blank" alt="email") bw.vbnm@gmail.com] 😁.
 </template>
@@ -10,3 +10,11 @@ export default {
   name: 'Intro'
 }
 </script>
+
+<style lang="scss" scoped>
+@media screen and (max-width: 30em){
+  .display {
+    font-size: 2.5rem;
+  }
+}
+</style>
