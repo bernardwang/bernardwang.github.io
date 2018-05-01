e3f6a2af12e0d8d20d938ab6c70cf9334372e028
Bernard Wang <bernard@bernard.wang>
2018-05-01 10:23:42 -0500

    About page content

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,12 +7,13 @@
           p.mt0 Interested in my resume? Grab a copy #[a(:href="resumeSrc" target="_blank" alt="resume") here]!
         div.fl.w-100.w-80-ns
           div.fl.w-100.w-50-ns
-            p.mt0 I care about building empathetic, socially conscious communities in tech. Specifically, I'm interested in applying my background in development and design to empower organizing and advance #[a(href="https://civichall.org/civicist/an-education-in-community-technology/" target="_blank" alt="community tech") community tech].
+            p.mt0 My work is shaped by my interests in technology, design, and social justice. Whether it's with civic tech, nonprofits or the arts, I'm excited to apply my varied background to create experiences and products that are empathetic, engaging, and mission driven.
           div.fl.w-100.w-50-ns
-            p.mt0 Most recently, I've been building  #[a(href="http://grove.ai" target="_blank" alt="Grove AI") Grove AI], which helps progressive campaigns and nonprofits organize using Facebook Messenger chatbots.
+            p.mt0 Most recently, I built #[a(href="http://grove.ai" target="_blank" alt="Grove AI") Grove AI], which helps progressive campaigns and nonprofits organize using Facebook Messenger chatbots.
             p.mt0 Check out more of my work on my #[a(href="https://github.com/bernardwang" target="_blank" alt="github") Github]!
       div.ml-fifth.w-100-ns.mt3
-        button(@click="showFun = !showFun").f5.lh-copy.mt0.pa0.bn.b.tl.pointer.bg-transparent.link {{ showFun?"-  less":"+ more" }} fun
+        button(@click="showFun = !showFun").lh-copy.mt0.pa0.bn.tl.pointer.bg-transparent.btn
+          p.b.link {{ showFun ? "-  less" : "+ more" }} fun
     transition(name="fade")
       AboutMore(v-if="showFun")
 </template>
@@ -34,3 +35,10 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.btn {
+  width: 6rem;
+}
+</style>
+
