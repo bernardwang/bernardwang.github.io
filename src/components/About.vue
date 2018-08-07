e7972899a603493bf8bf0a17a30654b4b32eebae
Bernard Wang <bernard@bernard.wang>
2018-08-07 16:26:13 -0500

    Update info

--- src/components/About.vue
+++ src/components/About.vue
@@ -7,7 +7,7 @@
           p.mt0 Interested in my resume? Grab a copy #[a(:href="resumeSrc" target="_blank" alt="resume") here]!
         div.fl.w-100.w-80-ns
           div.fl.w-100.w-50-ns
-            p.mt0 My work is shaped by my interests in technology, design, and social justice. Whether it's with civic tech, nonprofits or the arts, I'm excited to apply my varied background to create experiences and products that are empathetic, engaging, and mission driven.
+            p.mt0 My work is shaped by my interests in technology, design, and social justice. Whether it's with civic tech, nonprofits or the arts, I'm excited to apply my varied background to create experiences that are empathetic, engaging, and mission driven.
           div.fl.w-100.w-50-ns
             p.mt0 Most recently, I built #[a(href="http://grove.ai" target="_blank" alt="Grove AI") Grove AI], which helps progressive campaigns and nonprofits organize using Facebook Messenger chatbots.
             p.mt0 Check out more of my work on my #[a(href="https://github.com/bernardwang" target="_blank" alt="github") Github]!
--- src/components/Intro.vue
+++ src/components/Intro.vue
@@ -1,7 +1,9 @@
 <template lang="pug">
   section#intro
-    h1.w-80-l.w-100.display Hey 👋, my name is Bernard! I'm a <span class="hl">frontend</span><span class="hl">engineer</span> and <span class="hl">designer</span> based in NYC.<br/>
-    p.f4.w-90-l.w-100.mt0 Say hi on #[a(href="https://twitter.com/_bernardwang/" target="_blank" alt="twitter") Twitter] or email me at #[a(href="mailto:bw.vbnm@gmail.com" target="_blank" alt="email") bw.vbnm@gmail.com] 😁.
+    h1.w-80-l.w-100.display Hey 👋, my name is Bernard! I'm a <span class="hl">frontend</span><span class="hl">engineer</span> and <span class="hl">designer</span> based in Chicago.<br/>
+
+    p.f4.w-90-l.w-100.ma0 Currently working on #[a(href="https://healthCare.gov" target="_blank" alt="healthcare.gov") Healthcare.gov] at #[a(href="https://www.adhocteam.us" target="_blank" alt="Ad Hoc") Ad Hoc].
+    p.f4.w-90-l.w-100.mt0 Say hi on #[a(href="https://twitter.com/_bernardwang/" target="_blank" alt="twitter") Twitter] or email me at #[a(href="mailto:bw.vbnm@gmail.com" target="_blank" alt="email") bw.vbnm@gmail.com]! 😁
 </template>
 
 <script>
